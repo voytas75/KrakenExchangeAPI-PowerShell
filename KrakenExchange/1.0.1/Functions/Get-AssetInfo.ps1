df4748d531c0ba8076e50c1aea66e8acdfc6e0db
Wojciech Napierała <wnapierala@hotmail.com>
2023-04-05 06:28:04 +0200

    user-agent

--- KrakenExchange/1.0.1/Functions/Get-AssetInfo.ps1
+++ KrakenExchange/1.0.1/Functions/Get-AssetInfo.ps1
@@ -1,4 +1,21 @@
 function Get-AssetInfo {
+    <#
+    .SYNOPSIS
+    Retrieves information about a specific asset from the Kraken API.
+    
+    .DESCRIPTION
+    This function retrieves information about a specific asset (e.g. currency) from the Kraken API. It requires the asset symbol as input, and returns detailed information about the asset including its name, ticker, trading volume, etc.
+    
+    .PARAMETER Asset
+    The symbol of the asset for which information is to be retrieved.
+    
+    .EXAMPLE
+    Get-AssetInfo -Asset "XBT"
+    Retrieves information about the "XBT" asset (Bitcoin) from the Kraken API.
+    
+    .NOTES
+    This function requires internet access to connect to the Kraken API and retrieve asset information. Make sure your system has internet connectivity before using this function.
+    #>
     [CmdletBinding()]
     param (
         [Parameter(Mandatory = $true)]
@@ -7,7 +24,7 @@ function Get-AssetInfo {
     
     $AssetInfoMethod = "/0/public/Assets"
     $endpoint = "https://api.kraken.com"
-    $UserAgent = "PowershellScript/1.0"
+    $UserAgent = "Powershell Module KrakenExchange/1.0"
     $AssetInfoUrl = $endpoint + $AssetInfoMethod
 
     $AssetInfoParams = [ordered]@{ 
