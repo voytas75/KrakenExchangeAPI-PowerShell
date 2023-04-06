e90c6606eabf89db0e9f2f093bc60e0b04e3024d
Wojciech Napierała <wnapierala@hotmail.com>
2023-04-06 15:16:24 +0200

    add help

--- KrakenExchange/1.0.2/Functions/Get-KETradableAssetPair.ps1
+++ KrakenExchange/1.0.2/Functions/Get-KETradableAssetPair.ps1
@@ -1,8 +1,39 @@
 function Get-KETradableAssetPair {
+    <#
+    .SYNOPSIS
+    Retrieves information about a specific Kraken asset pair.
+
+    .DESCRIPTION
+    The Get-KETradableAssetPair function retrieves information about a specific Kraken asset pair, such as trading fees, leverage, and margin.
+
+    .PARAMETER Pair
+    The trading pair to retrieve information for. Default value is "XBTUSD".
+
+    .PARAMETER Info
+    The type of information to retrieve for the asset pair. Possible values are "info" (default), "leverage", "fees", and "margin".
+
+    .EXAMPLE
+    PS C:\> Get-KETradableAssetPair -Pair "XXBTZUSD" -Info "fees"
+    Retrieves trading fee information for the "XXBTZUSD" asset pair.
+
+    .EXAMPLE
+    PS C:\> Get-KETradableAssetPair
+    Retrieves general information about the "XBTUSD" asset pair.
+
+    .NOTES
+    The KrakenExchange PowerShell module is not affiliated with or endorsed by Kraken exchange.
+    Author: wnapierala [@] hotmail.com, chatGPT
+    Date: 04.2023
+
+    .LINK
+    For more information, see the Kraken API documentation:
+    https://docs.kraken.com/rest/#tag/Market-Data/operation/getTradableAssetPairs
+    #>
+
     [CmdletBinding()]
     param (
-        [Parameter(Mandatory = $true)]
-        [string]$Pair,
+        [Parameter()]
+        [string]$Pair = "XBTUSD",
 
         [Parameter()]
         [validateSet("info", "leverage", "fees", "margin")]
