6ccd4f73d9906e221d7611b183ec7c74af3352cc
Wojciech Napierała <wnapierala@hotmail.com>
2023-04-17 22:58:30 +0200

    help

--- KrakenExchange/Functions/Other/Set-KESignature.ps1
+++ KrakenExchange/Functions/Other/Set-KESignature.ps1
@@ -1,27 +1,21 @@
 function Set-KESignature {
-<#
-.SYNOPSIS
+    <#
+    .SYNOPSIS
     Calculates the signature for a Kraken API request.
 
-.DESCRIPTION
+    .DESCRIPTION
     Calculates the signature for a Kraken API request using the specified payload, URI, and API secret.
 
-.PARAMETER Payload
+    .PARAMETER Payload
     The payload for the API request, as an OrderedDictionary.
 
-.PARAMETER URI
+    .PARAMETER URI
     The URI for the API request, as a string.
 
-.PARAMETER api_secret
+    .PARAMETER api_secret
     The API secret for the Kraken account, as a string.
-.NOTES
-    Both [System.Security.Cryptography.HashAlgorithm]::Create('SHA256') and [System.Security.Cryptography.SHA256]::Create() create an instance of the SHA256 hashing algorithm provided by the .NET Framework's System.Security.Cryptography namespace. The difference between them is that the former creates an instance of the HashAlgorithm class and the latter creates an instance of the SHA256 class, which inherits from HashAlgorithm.
-    The HashAlgorithm class is an abstract base class that provides the basic functionality common to all hash algorithms. It defines the ComputeHash method, which computes the hash value for a given input, as well as other methods and properties that are common to all hash algorithms. When you create an instance of the SHA256 algorithm using [System.Security.Cryptography.HashAlgorithm]::Create('SHA256'), you are creating an instance of the SHA256Managed class, which is a concrete implementation of the SHA256 algorithm that inherits from HashAlgorithm.
-    On the other hand, when you create an instance of the SHA256 algorithm using [System.Security.Cryptography.SHA256]::Create(), you are creating an instance of the SHA256Cng class, which is a concrete implementation of the SHA256 algorithm that inherits directly from SHA256.
-    In general, it is recommended to use the specific algorithm class, such as SHA256, rather than the HashAlgorithm base class, as it provides a more strongly-typed interface and may have additional functionality specific to that algorithm. However, both methods should work fine for creating an instance of the SHA256 algorithm.
-    [System.Security.Cryptography.HMAC]::Create("HMACSHA512") - it works too.LINK
-    Specify a URI to a help page, this will show when Get-Help -Online is used.
-.EXAMPLE
+
+    .EXAMPLE
     PS C:\> $payload = [System.Collections.Specialized.OrderedDictionary]::new()
     PS C:\> $payload.Add("nonce", [int64]([DateTime]::UtcNow - (New-Object DateTime 1970, 1, 1, 0, 0, 0, 0, ([DateTimeKind]::Utc))).TotalMilliseconds
     PS C:\> $payload.Add("ordertype", "limit")
@@ -31,11 +25,17 @@ function Set-KESignature {
     PS C:\> $payload.Add("volume", "0.01")
     PS C:\> Set-APIKrakenSignature -Payload $payload -URI "/0/private/AddOrder" -api_secret "KrakenAPIsecret"
 
-.NOTES
+    .NOTES
     The KrakenExchange PowerShell module is not affiliated with or endorsed by Kraken exchange.
     Author: wnapierala [@] hotmail.com, chatGPT
     Date: 03.2023
-#>
+
+        Both [System.Security.Cryptography.HashAlgorithm]::Create('SHA256') and [System.Security.Cryptography.SHA256]::Create() create an instance of the SHA256 hashing algorithm provided by the .NET Framework's System.Security.Cryptography namespace. The difference between them is that the former creates an instance of the HashAlgorithm class and the latter creates an instance of the SHA256 class, which inherits from HashAlgorithm.
+    The HashAlgorithm class is an abstract base class that provides the basic functionality common to all hash algorithms. It defines the ComputeHash method, which computes the hash value for a given input, as well as other methods and properties that are common to all hash algorithms. When you create an instance of the SHA256 algorithm using [System.Security.Cryptography.HashAlgorithm]::Create('SHA256'), you are creating an instance of the SHA256Managed class, which is a concrete implementation of the SHA256 algorithm that inherits from HashAlgorithm.
+    On the other hand, when you create an instance of the SHA256 algorithm using [System.Security.Cryptography.SHA256]::Create(), you are creating an instance of the SHA256Cng class, which is a concrete implementation of the SHA256 algorithm that inherits directly from SHA256.
+    In general, it is recommended to use the specific algorithm class, such as SHA256, rather than the HashAlgorithm base class, as it provides a more strongly-typed interface and may have additional functionality specific to that algorithm. However, both methods should work fine for creating an instance of the SHA256 algorithm.
+    [System.Security.Cryptography.HMAC]::Create("HMACSHA512") - it works too
+    #>
 
     [CmdletBinding()]
     param (
